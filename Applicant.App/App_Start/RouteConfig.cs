2537ed4252a750cdf17a9a784e6624a78896c192
Schaul <schaulrotenberg@gmail.com>
2021-01-17 17:57:36 +0000

    Default action changed to List

--- Applicant.App/App_Start/RouteConfig.cs
+++ Applicant.App/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace JobApplicant.App
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "List", id = UrlParameter.Optional }
             );
         }
     }
